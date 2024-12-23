a25e191df1ec4c52dd59561997ad9f38fa88b60b
Sujan Tuladhar <sujan.tuladhar@soanitech.com>
2024-12-23 16:04:45 +0545

    Implement changes in button design

--- eventAnalytics/src/main/java/com/sujan/eventanalytics/SessionManagerImpl.kt
+++ eventAnalytics/src/main/java/com/sujan/eventanalytics/SessionManagerImpl.kt
@@ -1,20 +1,23 @@
 package com.sujan.eventanalytics
 
+import com.sujan.eventanalytics.models.AnalyticsEvent
+import com.sujan.eventanalytics.models.AnalyticsSession
+
 class SessionManagerImpl(private val storage: AnalyticsStorage) :
     SessionManager {
-    private var currentSession: com.sujan.eventanalytics.models.AnalyticsSession? = null
+    private var currentSession: AnalyticsSession? = null
 
     override fun startSession(sessionName: String) {
         if (currentSession != null) {
             throw IllegalStateException("A session is already in progress")
         }
-        currentSession = com.sujan.eventanalytics.models.AnalyticsSession(name = sessionName)
+        currentSession = AnalyticsSession(name = sessionName)
         storage.saveSession(currentSession!!)
     }
 
     override fun trackEvent(eventName: String, properties: Map<String, Any>) {
         val session = currentSession ?: throw IllegalStateException("No active session")
-        val event = com.sujan.eventanalytics.models.AnalyticsEvent(
+        val event = AnalyticsEvent(
             name = eventName,
             properties = properties
         )
